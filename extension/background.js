cc13d2e0743c84ce10c74d68b78cb069bc0a5b36
Rob Wu <gwnRob@gmail.com>
2013-09-30 23:18:42 +0200

    Show error message when error occurs

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,14 @@
-/* globals chrome */
+/* globals chrome, alert */
 'use strict';
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {
         file: 'toggle-anchors.js',
         allFrames: true
     }, function(result) {
+        if (chrome.runtime.lastError) {
+            alert(chrome.runtime.lastError.message);
+            return;
+        }
         if (!result || result.indexOf(true) === -1) {
             return;
         }
